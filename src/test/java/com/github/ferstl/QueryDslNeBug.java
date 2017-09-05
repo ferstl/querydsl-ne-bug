9c021c43dfa1ad453ab0f0a9fece3c7448a5d7b1
Stefan Ferstl <st.ferstl@gmail.com>
2017-09-05 22:02:27 +0200

    Reproduce bug in FetchableSubQuery#ne()

--- src/test/java/com/github/ferstl/QueryDslNeBug.java
+++ src/test/java/com/github/ferstl/QueryDslNeBug.java
@@ -1,7 +1,6 @@
 package com.github.ferstl;
 
 import java.sql.SQLException;
-import java.util.List;
 import javax.sql.DataSource;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +14,7 @@ import com.querydsl.sql.SQLQueryFactory;
 import static com.github.ferstl.QTableA.tableA;
 import static com.github.ferstl.QTableB.tableB;
 import static com.querydsl.sql.SQLExpressions.select;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
@@ -33,18 +33,17 @@ public class QueryDslNeBug {
 
   @Test
   public void test() throws SQLException {
-    List<String> result = this.queryFactory.query()
-        .select(tableA.description)
+    long result = this.queryFactory.query()
+        .select(tableA.id.count())
         .from(tableA)
         .where(
             select(tableB.category)
                 .from(tableB)
-                .where(tableB.aRef.eq(tableA.id).and(tableB.category.ne("cat-1")))
-                .exists())
-        .fetch();
+                .where(tableB.aRef.eq(tableA.id))
+                .ne("cat-1"))
+        .fetchOne();
 
-
-    System.out.println(result);
+    assertEquals(2L, result);
   }
 
 }
