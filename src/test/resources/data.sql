c977ec6ef5f11d129aa13e5ca7246cb810bfec81
Stefan Ferstl <st.ferstl@gmail.com>
2017-09-05 21:11:25 +0200

    Fix typo

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,4 +4,4 @@ INSERT INTO table_a(id, description) VALUES (3, 'entry 3');
 
 INSERT INTO table_b(a_ref, category) VALUES (1, 'cat-1');
 INSERT INTO table_b(a_ref, category) VALUES (2, 'cat-2');
-INSERT INTO table_b(a_ref, category) VALUES (3, 'val-2');
+INSERT INTO table_b(a_ref, category) VALUES (3, 'cat-2');
