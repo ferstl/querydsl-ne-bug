0e70b47ab964bc87cb56e4b868abb4a2f21a2b31
Stefan Ferstl <st.ferstl@gmail.com>
2017-09-05 21:11:51 +0200

    Cleanup

--- src/test/java/com/github/ferstl/QueryDslNeBug.java
+++ src/test/java/com/github/ferstl/QueryDslNeBug.java
@@ -1,38 +1,50 @@
 package com.github.ferstl;
 
 import java.sql.SQLException;
+import java.util.List;
 import javax.sql.DataSource;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.querydsl.sql.H2Templates;
-import com.querydsl.sql.SQLQuery;
 import com.querydsl.sql.SQLQueryFactory;
 
 import static com.github.ferstl.QTableA.tableA;
+import static com.github.ferstl.QTableB.tableB;
+import static com.querydsl.sql.SQLExpressions.select;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
 public class QueryDslNeBug {
 
   @Autowired
-  private JdbcOperations jdbcOperations;
-
-  @Autowired
   private DataSource dataSource;
+  private SQLQueryFactory queryFactory;
+
+  @Before
+  public void before() {
+    H2Templates h2Templates = new H2Templates();
+    this.queryFactory = new SQLQueryFactory(new com.querydsl.sql.Configuration(h2Templates), this.dataSource);
+
+  }
 
   @Test
   public void test() throws SQLException {
-    H2Templates h2Templates = new H2Templates();
-    new com.querydsl.sql.Configuration(h2Templates);
-    SQLQuery<Object> query = new SQLQuery<>(this.dataSource.getConnection(), h2Templates);
+    List<String> result = this.queryFactory.query()
+        .select(tableA.description)
+        .from(tableA)
+        .where(
+            select(tableB.category)
+                .from(tableB)
+                .where(tableB.aRef.eq(tableA.id).and(tableB.category.ne("cat-1")))
+                .exists())
+        .fetch();
 
-    SQLQueryFactory queryFactory = new SQLQueryFactory(new com.querydsl.sql.Configuration(h2Templates), this.dataSource);
-    System.out.println(queryFactory.query().select(tableA.description).from(tableA).fetch());
 
+    System.out.println(result);
   }
 
 }
